5f1103549c6c0d7da02c1f9e797a7a2996602a84
Abhishek Shankar <developer2@ideausher.com>
2025-08-23 02:50:36 +0530

    AS:bug fixes

--- app/page.jsx
+++ app/page.jsx
@@ -6,7 +6,7 @@ import { Card, CardContent } from "@/components/ui/card";
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
 import { useRouter } from "next/navigation";
-
+export const revalidate = 60;
 const categories = [
   "Good News",
   "Our Picks",
